c52a1e0007bf8b5d7234dc46c6f97d54ef9b7280
cmatos <cmatos@uolinc.com>
2025-04-25 10:50:58 -0300

    [master] unificando controle dos milisegundos: adicionando /top-countries

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -15,6 +15,7 @@ func main() {
 	mux.HandleFunc("/users", internal.UploadUsers).Methods("POST")
 	mux.HandleFunc("/users", internal.GetUsers).Methods("GET")
 	mux.HandleFunc("/superusers", internal.GetSuperUsers).Methods("GET")
+	mux.HandleFunc("/top-countries", internal.GetTopCountries).Methods("GET")
 
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(mux)); err != nil {
--- internal/users.go
+++ internal/users.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"runtime"
+	"sort"
 	"time"
 )
 
@@ -36,6 +37,11 @@ type User struct {
 	Logs    []logData `json:"logs"`
 }
 
+type countCountry struct {
+	Country string `json:"country"`
+	Count   int    `json:"total"`
+}
+
 type responseBody struct {
 	Timestamp     string `json:"timestamp"`
 	ExecutionTime int64  `json:"execution_time_ms"`
@@ -53,11 +59,15 @@ type responseUsers struct {
 	Data  []User `json:"data"`
 }
 
+type responseTopCountries struct {
+	responseBody
+	Countries []countCountry `json:"countries"`
+}
+
 var users []User
 
 func UploadUsers(w http.ResponseWriter, r *http.Request) {
-	var memStatus runtime.MemStats
-	start_time := time.Now()
+	memStatus, start_time := initCheck()
 
 	file, _, err := r.FormFile("file")
 	if err != nil {
@@ -80,11 +90,7 @@ func UploadUsers(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Println("Received users: ", len(users))
-	runtime.ReadMemStats(&memStatus)
-	duration := time.Since(start_time)
-	milliseconds := duration.Milliseconds()
-	info := fmt.Sprintf("Elapsed time = %vms. Total memory(KB) consumed = %v", milliseconds, memStatus.Sys/1024)
-
+	milliseconds, info := finishCheck(memStatus, start_time)
 	response := responseUploadUsers{
 		responseBody: responseBody{
 			Timestamp:     time.Now().Format(time.RFC3339),
@@ -101,20 +107,32 @@ func UploadUsers(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func GetUsers(w http.ResponseWriter, r *http.Request) {
+func initCheck() (runtime.MemStats, time.Time) {
 	var memStatus runtime.MemStats
 	start_time := time.Now()
+
+	return memStatus, start_time
+}
+
+func finishCheck(memStatus runtime.MemStats, start_time time.Time) (int64, string) {
+	runtime.ReadMemStats(&memStatus)
+	duration := time.Since(start_time)
+
+	milliseconds := duration.Milliseconds()
+	info := fmt.Sprintf("Elapsed time = %vms. Total memory(KB) consumed = %v", milliseconds, memStatus.Sys/1024)
+
+	return milliseconds, info
+}
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	memStatus, start_time := initCheck()
 
 	if len(users) == 0 {
 		http.Error(w, "No users found", http.StatusNotFound)
 		return
 	}
 
-	runtime.ReadMemStats(&memStatus)
-	duration := time.Since(start_time)
-	milliseconds := duration.Milliseconds()
-	info := fmt.Sprintf("Elapsed time = %vms. Total memory(KB) consumed = %v", milliseconds, memStatus.Sys/1024)
-
+	milliseconds, info := finishCheck(memStatus, start_time)
 	response := responseUsers{
 		responseBody: responseBody{
 			Timestamp:     time.Now().Format(time.RFC3339),
@@ -133,25 +151,15 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetSuperUsers(w http.ResponseWriter, r *http.Request) {
-	var memStatus runtime.MemStats
-	start_time := time.Now()
+	memStatus, start_time := initCheck()
 
 	if len(users) == 0 {
 		http.Error(w, "No users found", http.StatusNotFound)
 		return
 	}
 
-	superUsers := make([]User, 0)
-	for _, user := range users {
-		if user.Score >= 900 && user.Active {
-			superUsers = append(superUsers, user)
-		}
-	}
-
-	runtime.ReadMemStats(&memStatus)
-	duration := time.Since(start_time)
-	milliseconds := duration.Milliseconds()
-	info := fmt.Sprintf("Elapsed time = %vms. Total memory(KB) consumed = %v", milliseconds, memStatus.Sys/1024)
+	superUsers := findSuperUsers()
+	milliseconds, info := finishCheck(memStatus, start_time)
 	response := responseUsers{
 		responseBody: responseBody{
 			Timestamp:     time.Now().Format(time.RFC3339),
@@ -165,6 +173,63 @@ func GetSuperUsers(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(response); err != nil {
 		http.Error(w, "Failed to encode super users", http.StatusInternalServerError)
+		return
+	}
+}
+
+func findSuperUsers() []User {
+	superUsers := make([]User, 0)
+	for _, user := range users {
+		if user.Score >= 900 && user.Active {
+			superUsers = append(superUsers, user)
+		}
+	}
+
+	return superUsers
+}
+
+func GetTopCountries(w http.ResponseWriter, r *http.Request) {
+	memStatus, start_time := initCheck()
+
+	if len(users) == 0 {
+		http.Error(w, "No users found", http.StatusNotFound)
+		return
+	}
+
+	superUsers := findSuperUsers()
+	countries := make(map[string]int)
+	for _, superUser := range superUsers {
+		countries[superUser.Country]++
+	}
+
+	countriesList := make([]countCountry, 0)
+	for country, count := range countries {
+		countriesList = append(countriesList, countCountry{
+			Country: country,
+			Count:   count,
+		})
+	}
+
+	sort.Slice(countriesList, func(i, j int) bool {
+		return countriesList[i].Count > countriesList[j].Count
+	})
+	if len(countriesList) > 5 {
+		countriesList = countriesList[:5]
+	}
+
+	milliseconds, info := finishCheck(memStatus, start_time)
+	response := responseTopCountries{
+		responseBody: responseBody{
+			Timestamp:     time.Now().Format(time.RFC3339),
+			ExecutionTime: milliseconds,
+			Message:       info,
+		},
+		Countries: countriesList,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Failed to encode top countries", http.StatusInternalServerError)
 		return
 	}
 }
